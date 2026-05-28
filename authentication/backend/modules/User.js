1e024cae717a30ce36ec8d4057dd67aca769e088
CodeWithMB <mohan243200s@gmail.com>
2026-05-28 22:35:20 +0530

    added password hashing using bcrypt and async function

--- authentication/backend/modules/User.js
+++ authentication/backend/modules/User.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -16,3 +16,8 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 }, {timestamps: true});
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) next();
+    const salt = await bcrypt.genSalt(10);
+})
